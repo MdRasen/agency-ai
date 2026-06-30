c93af2e1ca15dbb26e13c3031f2b7e4831132c54
MdRasen <aamin.hossen99@gmail.com>
2026-07-01 00:40:11 +0600

    Navbar Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>App</div>;
+  //    get theme
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light",
+  );
+
+  return (
+    <div className="dark:bg-black relative">
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
+  );
 };
 
 export default App;
